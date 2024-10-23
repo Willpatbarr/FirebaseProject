1a824b73376052458e1a5219ebb0fd4a5008b415
Willpatbarr <145700970+Willpatbarr@users.noreply.github.com>
2024-10-23 14:41:49 -0600

    Initial Commit

---
+++ FirebaseProject/FirebaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseProjectApp.swift
+//  FirebaseProject
+//
+//  Created by William Barr on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirebaseProjectTests/FirebaseProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseProjectTests.swift
+//  FirebaseProjectTests
+//
+//  Created by William Barr on 10/23/24.
+//
+
+import Testing
+@testable import FirebaseProject
+
+struct FirebaseProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
